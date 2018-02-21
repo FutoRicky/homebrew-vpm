3292f6633ab63c6a979f88d3fdf9bd609e2dadf0
Ricardo Mercado <ricardoamercado@gmail.com>
2018-02-21 16:55:35 -0400

    Add some fixes to brew audit --strict formula problems

--- vpm.rb
+++ vpm.rb
@@ -1,9 +1,9 @@
 class Vpm < Formula
-  desc "Vim Plugin Manager implemented in go for the new native package loading feature in vim8."
+  desc "Vim Plugin Manager for the new native package loading feature in vim8"
   homepage "https://github.com/Waasi/vpm"
   url "https://github.com/Waasi/vpm/raw/master/vpm"
+  version "1.0.1"
   sha256 "539e3935236b49f0c462404c7ad0cf01d9b860746b1068121d96336a99c54988"
-  version "1.0.0"
 
   bottle :unneeded
 
@@ -11,4 +11,6 @@ class Vpm < Formula
     bin.install "vpm"
   end
 
+  test do
+  end
 end
