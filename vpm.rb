20aa0985d5688275ef4812ffa955d6a38fb7020e
Ricardo Mercado <ricardoamercado@gmail.com>
2018-02-21 16:21:15 -0400

    Add vpm formula

---
+++ vpm.rb
@@ -0,0 +1,14 @@
+class Vpm < Formula
+  desc "Vim Plugin Manager implemented in go for the new native package loading feature in vim8."
+  homepage "https://github.com/Waasi/vpm"
+  url "https://github.com/Waasi/vpm/raw/master/vpm"
+  sha256 "539e3935236b49f0c462404c7ad0cf01d9b860746b1068121d96336a99c54988"
+  version "1.0.0"
+
+  bottle :unneeded
+
+  def install
+    bin.install "vpm"
+  end
+
+end
